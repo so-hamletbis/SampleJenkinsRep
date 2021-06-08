07fd5e3eb4f9155bad09fc5fc40001507c4a392d
soham <www.soham.biswas11@gmail.com>
2021-06-08 10:00:06 +0530

    new sampleclass added

---
+++ SampleJavaProject/src/com/cg/samplejavaproj/SampleClass.java
@@ -0,0 +1,11 @@
+package com.cg.samplejavaproj;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEllo class");
+
+	}
+
+}
